b9285b55ec7a13dc67dbc84e647f28327e0b572e
Alberto S. Castaneda <alb2594@gmail.com>
2016-11-05 01:38:47 -0600

    menu y slider

---
+++ application/views/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ALECU</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximun-scale=1">
+    <link rel="shortcut icon" href="<?= base_url("assets/image/logo_01.jpg")?>" />
+    <link href=<?= base_url("assets/css/main.css")?> rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <header>
+        <nav>
+          <ul>
+            <li><div id="logo"><img src="<?= base_url("assets/image/logo_01.png"); ?>"  alt="logo alecu"></div></li>
+            <li><a href="#">INICIO</a></li>
+            <li><a href="#">NOSOTROS</a></li>
+            <li><a href="#">HISTORIA</a></li>
+            <li><a href="#">AREAS PROTEGIDAS</a></li>
+            <li><a href="#">CONTACTENOS</a></li>
+          </ul>
+        </nav>
+    </header>
+    <div class="fullScreen slider_container">
+      <div class="slideContainer">
+        <div class="slide fullScreen" data-background="<?= base_url("assets/image/DSC02097")?>"></div>
+        <div class="slide fullScreen" data-background="<?= base_url("assets/image/DSC02173")?>"></div>
+        <div class="slide fullScreen" data-background="<?= base_url("assets/image/DSC03117")?>"></div>
+      </div>
+    </div>
+    <div class="fullScreen">
+
+    </div>
+    <div class="fullScreen">
+
+    </div>
+    <div class="fullScreen">
+
+    </div>
+    <div class="fullScreen">
+
+    </div>
+
+    <script src="<?= base_url("assets/js/jquery-1.11.3.min.js")?>"></script>
+    <script src="<?= base_url("assets/js/main.js")?>"></script>
+  </body>
+</html>
---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+var pos = 0;
+
+$(document).on('ready' ,function() {
+  var width = $('.slider_container').width();
+  $('.slide').each(function(i,e){
+      addBackground(e,width,true);
+  });
+  intv = setInterval(handleClick,10000);
+});
+
+function addBackground(element,width,setSize){
+  if(!width) width = $('html').width();
+	if(setSize){
+		$(element).css({
+			'width': width,
+			'heigth': $('html').height()
+		});
+	}
+	var imagen = $(element).data('background');
+    $(element).css('background-image',"url("+(imagen+".jpg")+")");
+}
+
+function handleClick(){
+    var slide_target = 0;
+    if($(this).parent().hasClass('slider_controls')){
+        slide_target = $(this).index();
+        pos = slide_target;
+        clearInterval(intv);
+        intv = setInterval(handleClick,10000);
+    }
+    else{
+        pos++;
+        if(pos>=$('.slide').length){
+            pos = 0;
+        }
+        slide_target= pos;
+    }
+   /*$('.slideContainer').animate({
+       'margin-left':-(slide_target * $('.slider_container').width())+'px'
+   },'slow'); */
+    $('.slideContainer').fadeOut('slow',function(){
+		$(this).animate({
+			'margin-left':-(slide_target * $('.slider_container').width())+'px'
+		},'slow',function(){
+			$(this).fadeIn();
+		});
+	});
+}
